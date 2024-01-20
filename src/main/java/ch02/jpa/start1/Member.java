611d586352fd264dacac78fb80476d95728c1d12
박승찬 <seungchan141414@gmail.com>
2024-01-20 13:58:41 +0900

    p77 2장  매핑 에노테이션의 정보 추가

--- src/main/java/ch02/jpa/start1/Member.java
+++ src/main/java/ch02/jpa/start1/Member.java
@@ -9,13 +9,19 @@ import javax.persistence.*;  //**
 @Table(name="MEMBER")
 public class Member {
 
+    // 기본 키(PK)에 매핑한다.
+    // id 필드를 테이블의 ID 기본키 컬럼에 매핑한다.
+    // 식별자 필드
     @Id
     @Column(name = "ID")
     private String id;
 
+    // 필드를 컴럼에 매핑한다.
     @Column(name = "NAME")
     private String username;
 
+    // 매핑 정보가 없는 필드
+    // 필드 명이 age이르로 age컬럼으로 매핑
     private Integer age;
 
     public String getId() {
