dfd0b3151e4b64a5b3d081e50479c4cf36d1b292
박승찬 <seungchan141414@gmail.com>
2024-01-20 15:08:07 +0900

    p83 2장 EntityManager의 설정 주석 추가

--- src/main/java/ch02/jpa/start1/JpaMain.java
+++ src/main/java/ch02/jpa/start1/JpaMain.java
@@ -11,7 +11,11 @@ public class JpaMain {
     public static void main(String[] args) {
 
         //엔티티 매니저 팩토리 생성
+        // p83. JPA를 시작하려면 우선 persistence.xml의 설정 정보를 사용해서 엔티티 매니저 팩토리 생성
+        // 생성 비용이 크기 때문에 => 프로젝트 전반에 한번만 생성하고 공유해서 사용한다.
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        // p83. 엔티티 매니저는 등록/수정/삭제/조회를 할수 있다.
+        // 엔티티 매니저는 내부에 데이터소스(데이터베이스 커넥션)를 유지 하면서 데이터베이스와 통신
         EntityManager em = emf.createEntityManager(); //엔티티 매니저 생성
 
         EntityTransaction tx = em.getTransaction(); //트랜잭션 기능 획득
